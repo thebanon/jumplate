f373e62d0f12fe6fd439a54b019f7e629d998285
Tim Banon <izmbanon@gmail.com>
2020-03-14 14:58:13 -0400

    Imported framework

--- user.js
+++ user.js
@@ -15,12 +15,25 @@
 // @downloadURL  https://github.com/thebanon/jumplate/raw/master/user.js
 // ==/UserScript==
 
+/*var reset = document.createElement("link");
+reset.setAttribute("rel", "stylesheet");
+reset.setAttribute("type", "text/css");
+reset.setAttribute("href", "https://github.com/thebanon/jumplate/raw/master/reset.css");
+document.head.appendChild(reset);*/
+
+var reset = document.createElement("style").setAttribute("id", "jumplate-reset");
+document.head.appendChild(reset)
+var reset = `
+    body { display: none !important; }
+`;
+document.head.querySelector('#style').insertAdjacentHTML('beforeend',reset);
+
 var style = document.createElement("link");
 style.setAttribute("rel", "stylesheet");
 style.setAttribute("type", "text/css");
-style.setAttribute("href", "https://thebanon.github.io/jumplate/style.css");
+style.setAttribute("href", "https://github.com/thebanon/jumplate/raw/master/style.css");
 document.head.appendChild(style);
 
 var script = document.createElement("script");
-script.setAttribute("src", "https://thebanon.github.io/jumplate/script.js");
+script.setAttribute("src", "https://github.com/thebanon/jumplate/raw/master/script.js");
 document.head.appendChild(script);
