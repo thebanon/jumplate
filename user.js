34842b522ee23cfe830624258f85ddf1cbfd8ba1
Tim Banon <banon.cf@gmail.com>
2020-03-14 19:16:41 -0400

    Update user.js

--- user.js
+++ user.js
@@ -11,19 +11,23 @@
 // @grant        GM_listValues
 // @run-at       document-end
 // @license      MIT
-// @updateURL    https://github.com/thebanon/jumplate/raw/master/user.js
-// @downloadURL  https://github.com/thebanon/jumplate/raw/master/user.js
+// @updateURL    https://banon.cf/jumplate/user.js
+// @downloadURL  https://banon.cf/jumplate/user.js
 // ==/UserScript==
 
-var style = `<link href="https://github.com/thebanon/jumplate/raw/master/style.css" rel="stylesheet" type="text/css"></link>`;
-var script = `<script src="https://github.com/thebanon/jumplate/raw/master/user.js"></script>`;
+/*
+var reset = document.createElement("style").setAttribute("id", "jumplate-reset");
+document.head.appendChild(reset)
+var reset = ` body > * { all: unset; } `;
+document.head.querySelector('#style').insertAdjacentHTML('beforeend',reset);
+*/
 
-style ? document.head.insertAdjacentHTML('beforeend',style) : null;
-script ? document.head.insertAdjacentHTML('beforeend',script) : null;
+var style = document.createElement("link");
+style.setAttribute("rel", "stylesheet");
+style.setAttribute("type", "text/css");
+style.setAttribute("href", "https://banon.cf/jumplate/style.css");
+document.head.appendChild(style);
 
-function ajax(url, settings) { //console.log(url,settings);
-  return new Promise((resolve, reject) => { var req;
-    if(settings && settings.dataType === 'POST') { req = new Request(url, { method: 'POST', body: (settings.data ? JSON.stringify(settings.data) : null), headers: new Headers() }); } else { req = url; }
-    fetch(req).then(response => response.text()).then(res => { try { resolve(res); } catch(e) { resolve(e); } });
-  });
-}  
+var script = document.createElement("script");
+script.setAttribute("src", "https://banon.cf/jumplate/script.js");
+document.head.appendChild(script);
